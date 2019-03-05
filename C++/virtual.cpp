402444a7e86de0b746d3c2d41a373887addb9559
liyalei <liyl04@missfresh.cn>
2019-03-05 20:39:25 +0800

    虚继承demo

--- C++/virtual.cpp
+++ C++/virtual.cpp
@@ -1,55 +1,65 @@
 #include <iostream>
 
 using namespace std;
+
 //基类
-class D
-{
+class D {
 public:
-    D(){cout<<"D()"<<endl;}
+    D() { cout << "D()" << endl; }
+
     D(string strInfo) {
-    	cout << strInfo << endl; 
-		}
-    ~D(){cout<<"~D()"<<endl;}
+        cout << strInfo << endl;
+    }
+
+    ~D() { cout << "~D()" << endl; }
+
 protected:
     int d;
 };
 
-class B:virtual public D
-{
+class B : virtual public D {
 public:
-    B(){cout<<"B()"<<endl;}
-    ~B(){cout<<"~B()"<<endl;}
+    B() { cout << "B()" << endl; }
+
+    ~B() { cout << "~B()" << endl; }
+
 protected:
     int b;
 };
 
-class A:virtual public D
-{
+class A : virtual public D {
 public:
-    A(){cout<<"A()"<<endl;}
-    ~A(){cout<<"~A()"<<endl;}
+    A() { cout << "A()" << endl; }
+
+    ~A() { cout << "~A()" << endl; }
+
 protected:
     int a;
 };
 
-class C:public B, public A
-{
+class C : public B, public A {
 public:
-    C(){cout<<"C()"<<endl;}
-    ~C(){cout<<"~C()"<<endl;}
+    C() { cout << "C()" << endl; }
+
+    ~C() { cout << "~C()" << endl; }
+
 protected:
     int c;
 };
 
-int main()
-{
-	/*
+int main() {
+
     cout << "Hello World!" << endl;
     C c;   //D, B, A ,C
-    cout<<sizeof(c)<<endl;
-    */
+    cout << "int:" << sizeof(int) << endl;
+    cout << "D:" << sizeof(D) << endl;
+    cout << "B:" << sizeof(B) << endl;
+    cout << "A:" << sizeof(A) << endl;
+    cout << "C:" << sizeof(c) << endl;
+
     char test[] = "hello!\n";
     D *pD = new D(test);
+    cout << sizeof(pD) << endl;
     delete pD;
     return 0;
 }
