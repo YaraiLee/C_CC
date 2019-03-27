90c157017b0fcb6ec4cbc3611991624423a7e3ea
liyalei <liyl04@missfresh.cn>
2019-03-27 14:49:44 +0800

    添加虚函数表指针demo

---
+++ C++/vir_pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+  public:
+    int a;
+    virtual void myfun() {}
+};
+
+class B : public A
+{
+  public:
+    int b;
+};
+
+int main()
+{
+    B obj_b;
+    obj_b.a = 1;
+    obj_b.b = 2;
+    A *p = &obj_b;
+    cout << p << endl;
+    cout << &(p->a) << endl;
+    cout << p->a << '\t' << *(&(p->a) + 1) << endl; //*(&(p->a) + 1) --> obj_b.b，父类指针不能直接获取子类的变量
+}
