3f5d06cf232465d3c09a3abb201b59fa72a931db
yaleili <yaleili123@163.com>
2019-06-01 10:00:54 +0800

    fix

--- C++/template/Friend/Friend/Time.h
+++ C++/template/Friend/Friend/Time.h
@@ -2,7 +2,7 @@
 
 class Time
 {
-	friend void gPrintTime(Time &time);	//全局友元函数，可以放在Time任何访问限定符(public\privaet\protected)下，但是建议放在类定义的最前面
+	friend void gPrintTime(Time &time);	//全局友元函数，可以放在Time任何访问限定符(public\private\protected)下，但是建议放在类定义的最前面
 public:
 	Time(int hour, int min, int sec);
 	~Time();
