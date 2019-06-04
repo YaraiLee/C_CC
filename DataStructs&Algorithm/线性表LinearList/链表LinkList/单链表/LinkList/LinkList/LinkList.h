187a090d96b6fc15e69ac682f6571feba1d0f38e
yaleili <yaleili123@163.com>
2019-06-04 14:11:19 +0800

    comment

--- DataStructs&Algorithm/线性表LinearList/链表LinkList/单链表/LinkList/LinkList/LinkList.h
+++ DataStructs&Algorithm/线性表LinearList/链表LinkList/单链表/LinkList/LinkList/LinkList.h
@@ -8,19 +8,24 @@ public:
 	LinkList();
 	virtual ~LinkList();
 	bool isListEmpty();
+	//删除所有节点(头结点除外)
 	void clearList();
 	int getListLength();
-
+	//获取指定index处节点
 	bool getListElem(int index, Node* ptrNode) const;
+	//获取指定节点index
 	int getListElemLocate(const Node& rNode) const;
+	//遍历
 	void traverseList() const;
-
+	//头插
 	bool insertNodeHead(const Node& rNode);
+	//尾插
 	bool insertNodeTail(const Node& rNode);
-
+	//指定index插入节点
 	bool insertNode(int index, const Node& rNode);
+	//指定index删除节点
 	bool delListNode(int index, Node* ptrNode);
-
+	//链表倒置
 	void reverseList() const;
 private:
 	int m_iLength;
