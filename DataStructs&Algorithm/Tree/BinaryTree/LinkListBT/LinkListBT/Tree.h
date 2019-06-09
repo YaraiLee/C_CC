b83c53259c701fed3763eba5aa12c668bb24e3d3
yaleili <yaleili123@163.com>
2019-06-09 20:30:27 +0800

    comment

--- DataStructs&Algorithm/Tree/BinaryTree/LinkListBT/LinkListBT/Tree.h
+++ DataStructs&Algorithm/Tree/BinaryTree/LinkListBT/LinkListBT/Tree.h
@@ -8,7 +8,7 @@ public:
 	~Tree();
 	Node* searchNode(int nodeIndex);	//指定索引搜索节点
 	bool AddNode(int nodeIndex, int direction, Node* pNode);	//指定索引、左右，添加节点(随处添加，不仅限添加叶子节点)
-	bool DeleteNode(int index, Node* pNode);	//指定索引删除节点
+	bool DeleteNode(int index, Node* pNode);	//指定索引删除节点，包括该节点的所有子节点
 
 	void PreorderTraverse() const;	//前序遍历
 	void InorderTraverse() const;	//中序遍历
