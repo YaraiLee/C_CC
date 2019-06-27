7fdf3bf37538137ee1e36041ec78be9614ba025b
yaleili <yaleili123@163.com>
2019-06-27 12:23:30 +0800

    添加变量地址增加测试

--- C++/encapsulation/Teacher/Teacher/main.cpp
+++ C++/encapsulation/Teacher/Teacher/main.cpp
@@ -9,16 +9,23 @@ int main(void)
 	cout << t.getName() << " " << t.getAge() << endl;
 
 	Teacher *t1 = new Teacher("Lee", 29);
-	
+
 	cout << t1->getName() << " " << t1->getAge() << endl;
-	
+
 	Teacher t2(*t1);
 	cout << t2.getName() << " " << t2.getAge() << endl;
 
 	delete t1;
 	t1 = NULL;
 
+	//unsigned int *p = (unsigned int *)0x00000001;
+	//p = p + 5;
+	//cout << p << endl;
 
+	//cout << sizeof(unsigned int) << endl;
+
+	//cout << sizeof(long) << endl;
+	//cout << sizeof(void *) << endl;
 	system("pause");
 	return 0;
 }
