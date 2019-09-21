3804961befaa18a656199e7cd1a01248375907f9
YaraiLee <yaleili123@163.com>
2019-09-21 10:37:55 +0800

    comment

--- interview/EightQueues.cpp
+++ interview/EightQueues.cpp
@@ -6,27 +6,22 @@ namespace my {
 	static int arr[MAX_ROW][MAX_COL] = {0};
 
 	static int count = 0;
-	
+	//只需判断a[i][j]的上列直线、左右上对角线是否出现过1，
+	//左横直线不需要判断，因为判断方向是确定一行然后列从0开始，从左往右判断，失败则会重置为0，保证了左横不会出现1
 	bool check(int row, int col) {
-		//a[i][j]左横线判断
-		for (int j = 0; j <= col; j++) {
-			if (1 == arr[row][j]) {
-				return false;
-			}
-		}
-		//a[i][j]直上线判断
+		//a[i][j]上列线判断
 		for (int i = 0; i <= row; i++) {
 			if (arr[i][col] == 1) {
 				return false;
 			}
 		}
-		//a[i][j]左上斜线判断
+		//a[i][j]左上对角线判断
 		for (int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--) {
 			if (1 == arr[i][j]) {
 				return false;
 			}
 		}
-		//a[i][j]右上斜线判断
+		//a[i][j]右上对角线判断
 		for (int i = row-1, j = col+1; i >= 0 && j < MAX_COL; i--, j++) {
 			if (1 == arr[i][j]) {			
 				return false;
@@ -48,8 +43,9 @@ namespace my {
 		}
 		cout << endl;
 	}
-	//矩阵填充是按列优先，从左往右（从大到小），确定一列然后判断该列每行元素
-	//check只需判断该点所在行的上侧和列的左侧（包含该列、行）
+	//确定行坐标row，然后该行的列坐标col从0开始，判断a[row][col]左右上对角线和上直线是否出现1
+	//出现过，则col++,如果col == MAX_COL了，则退出该层递归，a[row][col]置为0
+	//未出现，则a[row][col]=1,对下一行递归判断，row == MAX_ROW则打印成功的矩阵
 	void findQueue(int row) {
 		if (row == MAX_ROW) {
 			count++;
@@ -57,7 +53,7 @@ namespace my {
 			return;
 		}
 		
-		for (int col = 0; col < MAX_COL; col++) {
+		for (int col = 0; col < MAX_COL; col++) {//深度回溯,递归算法
 			if (check(row, col)) {
 				arr[row][col] = 1;
 				findQueue(row+1);
