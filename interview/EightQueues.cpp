2f214e48c9b9dbcc419d7bdf71981327c158ef14
YaraiLee <yaleili123@163.com>
2019-09-21 09:43:40 +0800

    fix

--- interview/EightQueues.cpp
+++ interview/EightQueues.cpp
@@ -6,17 +6,27 @@ namespace my {
 	static int arr[MAX_ROW][MAX_COL] = {0};
 
 	static int count = 0;
+	
 	bool check(int row, int col) {
-		for (int i = 0; i < MAX_ROW; i++) {
+		//a[i][j]左横线判断
+		for (int j = 0; j <= col; j++) {
+			if (1 == arr[row][j]) {
+				return false;
+			}
+		}
+		//a[i][j]直上线判断
+		for (int i = 0; i <= row; i++) {
 			if (arr[i][col] == 1) {
 				return false;
 			}
 		}
+		//a[i][j]左上斜线判断
 		for (int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--) {
 			if (1 == arr[i][j]) {
 				return false;
 			}
 		}
+		//a[i][j]右上斜线判断
 		for (int i = row-1, j = col+1; i >= 0 && j < MAX_COL; i--, j++) {
 			if (1 == arr[i][j]) {			
 				return false;
@@ -38,9 +48,11 @@ namespace my {
 		}
 		cout << endl;
 	}
+	//矩阵填充是按列优先，从左往右（从大到小），确定一列然后判断该列每行元素
+	//check只需判断该点所在行的上侧和列的左侧（包含该列、行）
 	void findQueue(int row) {
 		if (row == MAX_ROW) {
-			my::count++;
+			count++;
 			printArr();
 			return;
 		}
