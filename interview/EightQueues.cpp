c62d4def7c559a139155b4f674afe354a544ddbd
YaraiLee <yaleili123@163.com>
2019-09-21 10:48:49 +0800

    comment

--- interview/EightQueues.cpp
+++ interview/EightQueues.cpp
@@ -7,7 +7,7 @@ namespace my {
 
 	static int count = 0;
 	//只需判断a[i][j]的上列直线、左右上对角线是否出现过1，
-	//左横直线不需要判断，因为判断方向是确定一行然后列从0开始，从左往右判断，失败则会重置为0，保证了左横不会出现1
+	//左横直线不需要判断，因为判断方向是从上到下，从左往右判断
 	bool check(int row, int col) {
 		//a[i][j]上列线判断
 		for (int i = 0; i <= row; i++) {
@@ -44,7 +44,7 @@ namespace my {
 		cout << endl;
 	}
 	//确定行坐标row，然后该行的列坐标col从0开始，判断a[row][col]左右上对角线和上直线是否出现1
-	//出现过，则col++,如果col == MAX_COL了，则退出该层递归，a[row][col]置为0
+	//出现过，则col++,如果col == MAX_COL了，则退出该层递归，上一层坐标a[row][col]置为0
 	//未出现，则a[row][col]=1,对下一行递归判断，row == MAX_ROW则打印成功的矩阵
 	void findQueue(int row) {
 		if (row == MAX_ROW) {
